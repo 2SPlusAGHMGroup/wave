17e890c8e749e9bc34ed5d6b848120fb04d37b1c
Frank Ren <renfeng.cn@gmail.com>
2013-11-23 00:46:55 +0800

    added full text search (search_type = solr);

---
+++ src/org/waveprotocol/box/server/waveserver/SolrPerUserWaveViewHandlerImpl.java
@@ -0,0 +1,314 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.waveprotocol.box.server.waveserver;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Multimap;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListenableFutureTask;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.RequestEntity;
+import org.apache.commons.httpclient.methods.StringRequestEntity;
+import org.apache.commons.lang.NotImplementedException;
+import org.apache.http.HttpStatus;
+import org.waveprotocol.box.common.DeltaSequence;
+import org.waveprotocol.box.common.DocumentConstants;
+import org.waveprotocol.box.server.waveserver.WaveBus.Subscriber;
+import org.waveprotocol.wave.model.document.operation.AnnotationBoundaryMap;
+import org.waveprotocol.wave.model.document.operation.Attributes;
+import org.waveprotocol.wave.model.document.operation.AttributesUpdate;
+import org.waveprotocol.wave.model.document.operation.DocOp;
+import org.waveprotocol.wave.model.document.operation.DocOpCursor;
+import org.waveprotocol.wave.model.document.operation.impl.InitializationCursorAdapter;
+import org.waveprotocol.wave.model.id.WaveId;
+import org.waveprotocol.wave.model.id.WaveletId;
+import org.waveprotocol.wave.model.id.WaveletName;
+import org.waveprotocol.wave.model.version.HashedVersion;
+import org.waveprotocol.wave.model.wave.ParticipantId;
+import org.waveprotocol.wave.model.wave.data.ReadableBlipData;
+import org.waveprotocol.wave.model.wave.data.ReadableWaveletData;
+
+import java.io.IOException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Lucene based implementation of {@link PerUserWaveViewHandler}.
+ * 
+ * @author yurize@apache.org (Yuri Zelikov)
+ */
+@Singleton
+public class SolrPerUserWaveViewHandlerImpl implements PerUserWaveViewHandler, Subscriber {
+
+  private static final Logger LOG = Logger
+      .getLogger(SolrPerUserWaveViewHandlerImpl.class.getName());
+
+  // TODO (Yuri Z.): Inject executor.
+  private static final Executor executor = Executors.newSingleThreadExecutor();
+
+  private final ReadableWaveletDataProvider waveletProvider;
+
+  /**
+   * Concatenates all of the text of the specified docops into a single String.
+   * 
+   * @param documentops the document operations to concatenate.
+   * @return A String containing the characters from the operations.
+   */
+  public static String readText(ReadableBlipData doc) {
+
+    final StringBuilder resultBuilder = new StringBuilder();
+
+    DocOp docOp = doc.getContent().asOperation();
+    docOp.apply(InitializationCursorAdapter.adapt(new DocOpCursor() {
+      @Override
+      public void characters(String s) {
+        resultBuilder.append(s);
+      }
+
+      @Override
+      public void annotationBoundary(AnnotationBoundaryMap map) {
+      }
+
+      @Override
+      public void elementStart(String type, Attributes attrs) {
+        if (type.equals(DocumentConstants.LINE)) {
+          resultBuilder.append("\n");
+        }
+      }
+
+      @Override
+      public void elementEnd() {
+      }
+
+      @Override
+      public void retain(int itemCount) {
+      }
+
+      @Override
+      public void deleteCharacters(String chars) {
+      }
+
+      @Override
+      public void deleteElementStart(String type, Attributes attrs) {
+      }
+
+      @Override
+      public void deleteElementEnd() {
+      }
+
+      @Override
+      public void replaceAttributes(Attributes oldAttrs, Attributes newAttrs) {
+      }
+
+      @Override
+      public void updateAttributes(AttributesUpdate attrUpdate) {
+      }
+    }));
+
+    return resultBuilder.toString();
+  }
+
+  @Inject
+  public SolrPerUserWaveViewHandlerImpl(ReadableWaveletDataProvider waveletProvider,
+      WaveletNotificationDispatcher notificationDispatcher) {
+    this.waveletProvider = waveletProvider;
+    notificationDispatcher.subscribe(this);
+  }
+
+  @Override
+  public ListenableFuture<Void> onParticipantAdded(final WaveletName waveletName,
+      ParticipantId participant) {
+
+    Preconditions.checkNotNull(waveletName);
+    Preconditions.checkNotNull(participant);
+
+    ListenableFutureTask<Void> task = new ListenableFutureTask<Void>(new Callable<Void>() {
+
+      @Override
+      public Void call() throws Exception {
+        ReadableWaveletData waveletData;
+        try {
+          waveletData = waveletProvider.getReadableWaveletData(waveletName);
+          updateIndex(waveletData);
+        } catch (WaveServerException e) {
+          LOG.log(Level.SEVERE, "Failed to update index for " + waveletName, e);
+          throw e;
+        }
+        return null;
+      }
+    });
+    executor.execute(task);
+    return task;
+  }
+
+  @Override
+  public ListenableFuture<Void> onParticipantRemoved(final WaveletName waveletName,
+      ParticipantId participant) {
+
+    Preconditions.checkNotNull(waveletName);
+    Preconditions.checkNotNull(participant);
+
+    ListenableFutureTask<Void> task = new ListenableFutureTask<Void>(new Callable<Void>() {
+
+      @Override
+      public Void call() throws Exception {
+        ReadableWaveletData waveletData;
+        try {
+          waveletData = waveletProvider.getReadableWaveletData(waveletName);
+          updateIndex(waveletData);
+        } catch (WaveServerException e) {
+          LOG.log(Level.SEVERE, "Failed to update index for " + waveletName, e);
+          throw e;
+        }
+        return null;
+      }
+    });
+    executor.execute(task);
+    return task;
+  }
+
+  @Override
+  public ListenableFuture<Void> onWaveInit(final WaveletName waveletName) {
+
+    ListenableFutureTask<Void> task = new ListenableFutureTask<Void>(new Callable<Void>() {
+
+      @Override
+      public Void call() throws Exception {
+        ReadableWaveletData waveletData;
+        try {
+          waveletData = waveletProvider.getReadableWaveletData(waveletName);
+          updateIndex(waveletData);
+        } catch (WaveServerException e) {
+          LOG.log(Level.SEVERE, "Failed to initialize index for " + waveletName, e);
+          throw e;
+        }
+        return null;
+      }
+    });
+    executor.execute(task);
+    return task;
+  }
+
+  private void updateIndex(ReadableWaveletData wavelet) throws IndexException {
+
+    Preconditions.checkNotNull(wavelet);
+
+    PostMethod postMethod = new PostMethod("http://localhost:8983/solr/update/json?commit=true");
+    // postMethod.setRequestHeader("Content-Type", "application/json");
+    try {
+      JsonArray docsJson = new JsonArray();
+
+      String waveId = wavelet.getWaveId().serialise();
+      String waveletId = wavelet.getWaveletId().serialise();
+      String modified = Long.toString(wavelet.getLastModifiedTime());
+
+      for (String docName : wavelet.getDocumentIds()) {
+        ReadableBlipData document = wavelet.getDocument(docName);
+
+        /*
+         * copied the method and disable replacing new lines with whitespaces
+         */
+        // String text = Snippets.collateTextForWavelet(wavelet);
+        String text = readText(document);
+
+        /*
+         * XXX i shouldn't skip empty wave/blips
+         */
+        // if (text.length() == 0) {
+        // }
+
+        JsonArray participantsJson = new JsonArray();
+        for (ParticipantId participant : wavelet.getParticipants()) {
+          String participantAddress = participant.toString();
+          participantsJson.add(new JsonPrimitive(participantAddress));
+        }
+
+        JsonObject docJson = new JsonObject();
+        docJson.addProperty("id", waveId + "/~/conv+root/" + docName);
+        docJson.addProperty("waveId_s", waveId);
+        docJson.addProperty("waveletId_s", waveletId);
+        docJson.addProperty("docName_s", docName);
+        docJson.addProperty("lmt_l", modified);
+        docJson.add("with_txt", participantsJson);
+        docJson.addProperty("text_t", text);
+        docJson.addProperty("in_ss", "inbox");
+
+        docsJson.add(docJson);
+      }
+
+      RequestEntity requestEntity =
+          new StringRequestEntity(docsJson.toString(), "application/json", "UTF-8");
+      postMethod.setRequestEntity(requestEntity);
+
+      HttpClient httpClient = new HttpClient();
+      int statusCode = httpClient.executeMethod(postMethod);
+      if (statusCode != HttpStatus.SC_OK) {
+        throw new IndexException(waveId);
+      }
+
+      LOG.fine(postMethod.getResponseBodyAsString());
+
+      /*
+       * FIXME update solr index
+       */
+    } catch (IOException e) {
+      throw new IndexException(String.valueOf(wavelet.getWaveletId()), e);
+    } finally {
+      postMethod.releaseConnection();
+    }
+
+    return;
+  }
+
+  @Override
+  public Multimap<WaveId, WaveletId> retrievePerUserWaveView(ParticipantId user) {
+    throw new NotImplementedException(
+        "See org.waveprotocol.box.server.waveserver.SolrSearchProviderImpl.search(ParticipantId, String, int, int)");
+  }
+
+  @Override
+  public void waveletUpdate(final ReadableWaveletData wavelet, DeltaSequence deltas) {
+    updateIndex(wavelet);
+  }
+
+  @Override
+  public void waveletCommitted(WaveletName waveletName, HashedVersion version) {
+    /*
+     * XXX don't update index here to prevent lock
+     */
+    // try {
+    // ReadableWaveletData waveletData =
+    // waveletProvider.getReadableWaveletData(waveletName);
+    // updateIndex(waveletData);
+    // } catch (WaveServerException e) {
+    // LOG.log(Level.SEVERE, "Failed to update index for " + waveletName, e);
+    // }
+  }
+}
---
+++ src/org/waveprotocol/box/server/waveserver/SolrSearchProviderImpl.java
@@ -0,0 +1,232 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.waveprotocol.box.server.waveserver;
+
+import static org.waveprotocol.box.server.waveserver.IndexFieldType.WAVEID;
+import static org.waveprotocol.box.server.waveserver.IndexFieldType.WAVELETID;
+
+import com.google.common.base.Function;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import com.google.wave.api.SearchResult;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.URI;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.http.HttpStatus;
+import org.waveprotocol.box.server.CoreSettings;
+import org.waveprotocol.box.server.util.WaveletDataUtil;
+import org.waveprotocol.box.server.waveserver.QueryHelper.InvalidQueryException;
+import org.waveprotocol.wave.model.id.IdUtil;
+import org.waveprotocol.wave.model.id.WaveId;
+import org.waveprotocol.wave.model.id.WaveletId;
+import org.waveprotocol.wave.model.id.WaveletName;
+import org.waveprotocol.wave.model.wave.InvalidParticipantAddress;
+import org.waveprotocol.wave.model.wave.ParticipantId;
+import org.waveprotocol.wave.model.wave.ParticipantIdUtil;
+import org.waveprotocol.wave.model.wave.data.ReadableWaveletData;
+import org.waveprotocol.wave.model.wave.data.WaveViewData;
+import org.waveprotocol.wave.model.wave.data.impl.WaveViewDataImpl;
+import org.waveprotocol.wave.util.logging.Log;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URLEncoder;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Search provider that reads user specific info from user data wavelet.
+ * 
+ * @author yurize@apache.org (Yuri Zelikov)
+ */
+public class SolrSearchProviderImpl implements SearchProvider {
+
+  private static final Log LOG = Log.get(SolrSearchProviderImpl.class);
+
+  private final WaveDigester digester;
+  private final WaveMap waveMap;
+
+  private final ParticipantId sharedDomainParticipantId;
+
+  private final PerUserWaveViewProvider waveViewProvider;
+
+  @Inject
+  public SolrSearchProviderImpl(@Named(CoreSettings.WAVE_SERVER_DOMAIN) final String waveDomain,
+      WaveDigester digester, final WaveMap waveMap, PerUserWaveViewProvider userWaveViewProvider) {
+    this.digester = digester;
+    this.waveMap = waveMap;
+    this.waveViewProvider = userWaveViewProvider;
+    sharedDomainParticipantId = ParticipantIdUtil.makeUnsafeSharedDomainParticipantId(waveDomain);
+  }
+
+  @Override
+  public SearchResult search(final ParticipantId user, String query, int startAt, int numResults) {
+    LOG.fine("Search query '" + query + "' from user: " + user + " [" + startAt + ", "
+        + (startAt + numResults - 1) + "]");
+
+    Multimap<WaveId, WaveletId> currentUserWavesView = HashMultimap.create();
+
+    /*
+     * http://localhost:8983/solr/select?wt=json&q=*:*
+     */
+    String q = "waveId_s:[* TO *]" //
+        + " AND waveletId_s:[* TO *]" //
+        + " AND docName_s:[* TO *]" //
+        + " AND lmt_l:[* TO *]" //
+        + " AND with_txt:[* TO *]" //
+        + " AND text_t:[* TO *]" //
+        + " AND in_ss:[* TO *]";
+
+    if (query.length() == 0) {
+      query = "*:*";
+    }
+    String fq =
+        "{!lucene q.op=AND df=text_t}"
+            + query.replaceAll("\\bin:", "in_ss:").replaceAll("\\bwith:", "with_txt:");
+
+    GetMethod getMethod = new GetMethod();
+    try {
+      // getMethod.setURI(new URI("http://localhost:8983/solr/select?wt=json&q="
+      // + URLEncoder.encode(q, "UTF-8")));
+      getMethod.setURI(new URI("http://localhost:8983/solr/select?wt=json&q=" + q + "&fq=" + fq,
+          false));
+
+      HttpClient httpClient = new HttpClient();
+      int statusCode = httpClient.executeMethod(getMethod);
+      if (statusCode != HttpStatus.SC_OK) {
+        LOG.warning("Failed to execute query: " + query);
+        return digester.generateSearchResult(user, query, null);
+      }
+
+      JsonObject json =
+          new JsonParser().parse(new InputStreamReader(getMethod.getResponseBodyAsStream()))
+              .getAsJsonObject();
+      JsonObject responseJson = json.getAsJsonObject("response");
+      JsonArray docsJson = responseJson.getAsJsonArray("docs");
+      for (int i = 0; i < docsJson.size(); i++) {
+        JsonObject docJson = docsJson.get(i).getAsJsonObject();
+        WaveId waveId = WaveId.deserialise(docJson.getAsJsonPrimitive("waveId_s").getAsString());
+        WaveletId waveletId =
+            WaveletId.deserialise(docJson.getAsJsonPrimitive("waveletId_s").getAsString());
+        currentUserWavesView.put(waveId, waveletId);
+      }
+
+    } catch (IOException e) {
+      LOG.warning("Failed to execute query: " + query);
+      return digester.generateSearchResult(user, query, null);
+    } finally {
+      getMethod.releaseConnection();
+    }
+
+
+    Map<WaveId, WaveViewData> results = filterWavesViewBySearchCriteria(currentUserWavesView);
+
+    if (LOG.isFineLoggable()) {
+      for (Map.Entry e : results.entrySet()) {
+        LOG.fine("filtered results contains: " + e.getKey());
+      }
+    }
+
+    Collection<WaveViewData> searchResult = results.values();
+    LOG.info("Search response to '" + query + "': " + searchResult.size() + " results, user: "
+        + user);
+    return digester.generateSearchResult(user, query, searchResult);
+  }
+
+
+  private Map<WaveId, WaveViewData> filterWavesViewBySearchCriteria(
+      Multimap<WaveId, WaveletId> currentUserWavesView) {
+    // Must use a map with stable ordering, since indices are meaningful.
+    Map<WaveId, WaveViewData> results = Maps.newLinkedHashMap();
+
+    // Loop over the user waves view.
+    for (WaveId waveId : currentUserWavesView.keySet()) {
+      Collection<WaveletId> waveletIds = currentUserWavesView.get(waveId);
+      WaveViewData view = null; // Copy of the wave built up for search hits.
+      for (WaveletId waveletId : waveletIds) {
+        WaveletContainer waveletContainer = null;
+        WaveletName waveletname = WaveletName.of(waveId, waveletId);
+
+        // TODO (alown): Find some way to use isLocalWavelet to do this
+        // properly!
+        try {
+          if (LOG.isFineLoggable()) {
+            LOG.fine("Trying as a remote wavelet");
+          }
+          waveletContainer = waveMap.getRemoteWavelet(waveletname);
+        } catch (WaveletStateException e) {
+          LOG.severe(String.format("Failed to get remote wavelet %s", waveletname.toString()), e);
+        } catch (NullPointerException e) {
+          // This is a fairly normal case of it being a local-only wave.
+          // Yet this only seems to appear in the test suite.
+          // Continuing is completely harmless here.
+          LOG.info(
+              String.format("%s is definitely not a remote wavelet. (Null key)",
+                  waveletname.toString()), e);
+        }
+
+        if (waveletContainer == null) {
+          try {
+            if (LOG.isFineLoggable()) {
+              LOG.fine("Trying as a local wavelet");
+            }
+            waveletContainer = waveMap.getLocalWavelet(waveletname);
+          } catch (WaveletStateException e) {
+            LOG.severe(String.format("Failed to get local wavelet %s", waveletname.toString()), e);
+          }
+        }
+
+        // TODO (Yuri Z.) This loop collects all the wavelets that match the
+        // query, so the view is determined by the query. Instead we should
+        // look at the user's wave view and determine if the view matches the
+        // query.
+        try {
+          if (waveletContainer == null) {
+            LOG.fine("----doesn't match: " + waveletContainer);
+            continue;
+          }
+          if (view == null) {
+            view = WaveViewDataImpl.create(waveId);
+          }
+          // Just keep adding all the relevant wavelets in this wave.
+          view.addWavelet(waveletContainer.copyWaveletData());
+        } catch (WaveletStateException e) {
+          LOG.warning("Failed to access wavelet " + waveletContainer.getWaveletName(), e);
+        }
+      }
+      if (view != null) {
+        results.put(waveId, view);
+      }
+    }
+    return results;
+  }
+}
